2577996c72179755a61dee2968a93566e530874a
LingyongWang <lwang84@illinois.edu>
2013-02-01 23:41:19 -0500

    camera controller

---
+++ MHackberrithon/MHackberrithon/MHCameraController.h
@@ -0,0 +1,13 @@
+//
+//  MHCameraController.h
+//  MHackberrithon
+//
+//  Created by Lingyong Wang on 2/1/13.
+//  Copyright (c) 2013 Hackberry. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MHCameraController : UIViewController
+
+@end
---
+++ MHackberrithon/MHackberrithon/MHCameraController.m
@@ -0,0 +1,38 @@
+//
+//  MHCameraController.m
+//  MHackberrithon
+//
+//  Created by Lingyong Wang on 2/1/13.
+//  Copyright (c) 2013 Hackberry. All rights reserved.
+//
+
+#import "MHCameraController.h"
+
+@interface MHCameraController ()
+
+@end
+
+@implementation MHCameraController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
