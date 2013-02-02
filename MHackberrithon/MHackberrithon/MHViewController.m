bbb876a359befae2964161f5768ede06e1844269
LingyongWang <lwang84@illinois.edu>
2013-02-01 23:35:21 -0500

    First Commit

---
+++ MHackberrithon/MHackberrithon/MHViewController.m
@@ -0,0 +1,29 @@
+//
+//  MHViewController.m
+//  MHackberrithon
+//
+//  Created by Lingyong Wang on 2/1/13.
+//  Copyright (c) 2013 Hackberry. All rights reserved.
+//
+
+#import "MHViewController.h"
+
+@interface MHViewController ()
+
+@end
+
+@implementation MHViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
